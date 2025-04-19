ef33e55338d859e43c32c90cfca44a86765e3da0
Prashant20nov2003 <prashanttbhardwajj@gmail.com>
2025-04-20 01:13:52 +0530

    added bar progress for uploads

--- lib/raffley_web/router.ex
+++ lib/raffley_web/router.ex
@@ -57,8 +57,6 @@ defmodule RaffleyWeb.Router do
       live "/charities/new", CharityLive.Form, :new
       live "/charities/:id", CharityLive.Show, :show
       live "/charities/:id/edit", CharityLive.Form, :edit
-
-      live "/dashboard", DashboardLive
     end
   end
 
