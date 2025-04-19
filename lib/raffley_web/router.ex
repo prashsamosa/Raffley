4b2b1853e4ec1e5ecdd202e0378fc8bb8ab6e5cc
Prashant20nov2003 <prashanttbhardwajj@gmail.com>
2025-04-19 10:55:28 +0000

    rule controller

--- lib/raffley_web/router.ex
+++ lib/raffley_web/router.ex
@@ -29,6 +29,7 @@ defmodule RaffleyWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    get "/rules", RuleController, :index
   end
 
   # Other scopes may use custom stacks.
